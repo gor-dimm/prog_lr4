91b19eac766ab44c1a6463400cc457410ea2c42f
gor-dimm <booo130502@gmail.com>
2021-11-09 14:26:52 +0300

    Индивидуальное задание

--- ind.py
+++ ind.py
@@ -5,20 +5,18 @@
 # виде суммы других натуральных чисел.
 
 
-n = int(input("Введите натуральное число: "))
+if __name__ == 'main':
+    def step(n, L):
+        if n != 0:
+            if n >= L[len(L) - 1]:
+                for i in range(max(1, L[len(L) - 1]), n + 1):
+                    step(n - i, L + [i])
 
-
-def step(n, L):
-    if n != 0:
-        if n >= L[len(L) - 1]:
-            for i in range(max(1, L[len(L) - 1]), n + 1):
-                step(n - i, L + [i])
-
-    else:
-        print(L[1], end="")
-        for i in L[2:]:
-            print('+{0}'.format(i), end="")
+            else:
+                print(L[1], end="")
+            for i in L[2:]:
+                print('+{0}'.format(i), end="")
         print()
 
-
+n = int(input("Введите натуральное число: "))
 step(n, [0])
